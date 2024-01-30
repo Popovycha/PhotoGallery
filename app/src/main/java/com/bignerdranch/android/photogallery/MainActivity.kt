0e69b55ce5830be65c6780606ecffc079eee39b9
popovycha <artem220397@gmail.com>
2024-01-30 17:36:23 -0500

    1st commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.photogallery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoRepository.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.photogallery
+
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import com.bignerdranch.android.photogallery.api.GalleryItem
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+
+private const val BASE_URL = "https://api.flickr.com/"
+
+class PhotoRepository {
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+    private val flickrApi: FlickrApi = retrofit.create()
+
+    suspend fun fetchPhotos(): List<GalleryItem> =
+        flickrApi.fetchPhotos().photos.galleryItems
+}
